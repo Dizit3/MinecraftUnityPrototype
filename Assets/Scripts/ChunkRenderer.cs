b9b22369b66876cb094b30e0889ad1cb6ebe61ab
Dizit3 <dizit3@gmail.com>
2022-12-26 12:32:07 +0200

    FirstCommit

---
+++ Assets/Scripts/ChunkRenderer.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(MeshFilter),typeof(MeshRenderer))]
+public class ChunkRenderer : MonoBehaviour
+{
+
+    private void Start()
+    {
+        
+    }
+
+
+}
